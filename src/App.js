a47c488a605ff0729f02e89938827090234442cb
Allan Leung <allanleung@Allans-MacBook-Pro.local>
2019-06-12 16:23:45 -0700

    Working on routes and trying to get Navbar to work

--- src/App.js
+++ src/App.js
@@ -1,42 +1,28 @@
-import React, { Component } from 'react';
-import {Switch,Route} from 'react-router-dom';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {Switch, Route} from 'react-router-dom';
+import logo from "./logo.svg";
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from './Component/Navbar';
-import ProductList from './Component/ProductList';
-import Details from './Component/Details';
-import Cart from './Component/Cart';
-import Default from "./Component/Default";
+import Navbar from "./components/Navbar"
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from "./components/Default";
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <React.Fragment>
-            <Navbar/>
-            <Switch>
-                <Route exact path="/" component={ProductList} />
-                <Route path="/details" component={Details} />
-                <Route path="/cart" component={Cart}/>
-                <Route component={Default}/>
-            </Switch>
-        </React.Fragment>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <React.Fragment>
+                <Navbar/>
+                <Switch>
+                    <Route exact path="/" components={ProductList}/>
+                    <Route path="/details" components={Details}/>
+                    <Route path="/cart" components={Cart}/>
+                    <Route components={Default}/>
+                </Switch>
+            </React.Fragment>
+        );
+    }
 }
-
-/*
-<div className="container">
-    <div className="row">
-    <div className="col-6"> column number one </div>
-<div className="col-6"> <span>
-                <i className="fas fa-home" />
-            </span>
-</div>
-</div>
-</div>
-*/
 
 export default App;
---
+++ src/context.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { storeProducts, detailProduct } from "./data";
+import Details from "./components/Details";
+
+//create context object, has provider and consumer
+const ProductContext = React.createContext();
+
+class ProducerProvider extends Component {
+    state = {
+        products: storeProducts, detailProduct: detailProduct
+    }
+    handleDetail = () => {
+        console.log('lhello from details');
+}
+addToCart = () =>{
+    console.log('lhello from add to Cart');
+}
+    render() {
+        return (
+            <ProductContext.Provider value={{
+                ...this.state,
+                handleDetails: this.handleDetail,
+                addToCart: this.addToCart
+            }}>
+                {this.props.children}
+            </ProductContext.Provider>
+        );
+    }
+}
+
+const ProductConsumer = ProductContext.Consumer;
+
+export {ProducerProvider, ProductConsumer}
