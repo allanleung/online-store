793caabb924f320501afe74b6fe0d10a9d6622ed
Allan Leung <allanleung@Allans-MacBook-Pro.local>
2019-06-12 11:37:23 -0700

    first commit, got components and routes setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {Switch,Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './Component/Navbar';
+import ProductList from './Component/ProductList';
+import Details from './Component/Details';
+import Cart from './Component/Cart';
+import Default from "./Component/Default";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <React.Fragment>
+            <Navbar/>
+            <Switch>
+                <Route exact path="/" component={ProductList} />
+                <Route path="/details" component={Details} />
+                <Route path="/cart" component={Cart}/>
+                <Route component={Default}/>
+            </Switch>
+        </React.Fragment>
+      </div>
+    )
+  }
 }
+
+/*
+<div className="container">
+    <div className="row">
+    <div className="col-6"> column number one </div>
+<div className="col-6"> <span>
+                <i className="fas fa-home" />
+            </span>
+</div>
+</div>
+</div>
+*/
 
 export default App;
